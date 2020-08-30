457686526694cfee70062402247e39082d555a4f
Valentine Valyaeff <valentine.valyaeff@gmail.com>
2020-08-30 13:59:11 +0300

    Update rust-toolchain

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,8 @@
 #![feature(generator_trait)]
 #![feature(generators)]
 #![feature(or_patterns)]
-#![warn(missing_docs)]
+#![feature(unsafe_block_in_unsafe_fn)]
+#![warn(missing_docs, unsafe_op_in_unsafe_fn)]
 #![warn(clippy::pedantic)]
 #![allow(
     clippy::cast_possible_truncation,
