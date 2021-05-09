c0fcb408290aa1d00cc5b9e02cd6d05833e5f4ce
Valentine Valyaeff <valentine.valyaeff@gmail.com>
2021-05-09 14:35:39 +0300

    Version bump

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! # Documentation
 //!
 //! - [Drone Book](https://book.drone-os.com/)
-//! - [API documentation](https://api.drone-os.com/drone-svd/0.14/)
+//! - [API documentation](https://api.drone-os.com/drone-svd/0.15/)
 //!
 //! # Usage
 //!
@@ -11,7 +11,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! drone-svd = { version = "0.14.0" }
+//! drone-svd = { version = "0.15.0" }
 //! ```
 
 #![feature(bool_to_option)]
